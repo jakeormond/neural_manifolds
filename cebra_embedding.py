93919569c2bec85b59b404af2010c82e551f49ec
Jake Ormond <jakeo@hpc-gw1.hpc.swc.ucl.ac.uk>
2024-03-18 15:00:50 +0000

    modified paths for scratch

--- cebra_embedding.py
+++ cebra_embedding.py
@@ -49,32 +49,34 @@ def plot_embeddings(embeddings):
 if __name__ == "__main__":
     animal = 'Rat46'
     session = '19-02-2024'
-    data_dir = get_data_dir(animal, session)
+    # data_dir = get_data_dir(animal, session)
+
+    data_dir = '/ceph/scratch/jakeo/'
 
     goal = 52
     window_size = 100
 
 
     ############ LOAD POSITIONAL DATA ################
-    dlc_dir = os.path.join(data_dir, 'deeplabcut')
+    #  = os.path.join(data_dir, 'deeplabcut')
     labels_file_name = f'labels_goal{goal}_ws{window_size}'
     # load numpy array of labels
-    labels = np.load(os.path.join(dlc_dir, labels_file_name + '.npy'))
+    labels = np.load(os.path.join(data_dir, labels_file_name + '.npy'))
     # keep only the first 2 columns
     labels = labels[:, :2]
 
     ############ LOAD SPIKE DATA #####################
     # load numpy array of neural data
-    spike_dir = os.path.join(data_dir, 'spike_sorting')
+    # spike_dir = os.path.join(data_dir, 'spike_sorting')
     inputs_file_name = f'inputs_goal{goal}_ws{window_size}'
-    inputs = np.load(os.path.join(spike_dir, inputs_file_name + '.npy'))
+    inputs = np.load(os.path.join(data_dir, inputs_file_name + '.npy'))
 
     # load convert inputs to torch tensor
     inputs = torch.tensor(inputs, dtype=torch.float32)  
 
     ########### TRAIN THE CEBRA MODEL ###############
-    cebra_model_dir = os.path.join(data_dir, 'cebra')
-    
+    # cebra_model_dir = os.path.join(data_dir, 'cebra')
+    cebra_model_dir = data_dir
     
     max_iterations = 10000 #default is 5000.
 
@@ -112,7 +114,8 @@ if __name__ == "__main__":
         cebra_file_name = f'cebra_posdir3_model_goal{goal}_ws{window_size}_position_fold{i+1}.pt'
         cebra_file_path = os.path.join(cebra_model_dir, cebra_file_name)
         # need to convert to double backslashes
-        cebra_file_path = cebra_file_path.replace("/", "\\")        
+        # cebra_file_path = cebra_file_path.replace("/", "\\")        
+        cebra_file_path = cebra_file_path.replace("\\", "/")        
         cebra_posdir3_model.save(cebra_file_path)
 
         ########## LOAD THE CEBRA MODEL ################
@@ -155,60 +158,11 @@ if __name__ == "__main__":
     fig_name = cebra_file_name.replace(".pt", ".png")
     
     # save fig to the cebra directory
-    fig_dir = os.path.join(data_dir, 'cebra')
+    # fig_dir = os.path.join(data_dir, 'cebra')
+    fig_dir = data_dir
     fig_path = os.path.join(fig_dir, fig_name)
     plt.savefig(fig_path)
 
     pass
-
-
-
-
-    # x is every 10th value of labels[:,0]
-    x = labels[::10,0]
-    y = labels[::10,1]
-
-    # convert x and y into vectors of integers between 0 and 255
-    x_int = np.interp(x, (x.min(), x.max()), (0, 255)).astype(int)
-    y_int = np.interp(y, (y.min(), y.max()), (0, 255)).astype(int)
-
-    embedding_data = cebra_posdir3[::10,:]
-
-    colormap = colormap_2d()  
-    # plt.imshow(colormap)  
-    color_data = colormap[x_int, y_int]
-
-    fig = plt.figure(figsize = (24,4), dpi = 100)
-    ax1 = fig.add_subplot(151, projection='3d')
-    # plot embedding_data using the color_data
-    ax1.scatter(embedding_data[:,0], embedding_data[:,1], embedding_data[:,2], c=color_data, s=1)
-
-    ax2 = fig.add_subplot(152, projection='3d')
-    # plot embedding_data using the color_data
-    ax2.scatter(embedding_data[:,0], embedding_data[:,1], embedding_data[:,2], c=color_data, s=1)
-
-    ax3 = fig.add_subplot(153, projection='3d')
-    # plot embedding_data using the color_data
-    ax3.scatter(embedding_data[:,0], embedding_data[:,1], embedding_data[:,2], c=color_data, s=1)
-
-    ax4 = fig.add_subplot(154, projection='3d')
-    # plot embedding_data using the color_data
-    ax4.scatter(embedding_data[:,0], embedding_data[:,1], embedding_data[:,2], c=color_data, s=1)
-
-    # for the second subplot: plt.imshow(data)
-    ax5 = fig.add_subplot(155)
-    ax5.imshow(colormap)
-
-    # fig_name = cebra_file_path = cebra_file_name.replace(".pt", ".png")
-    fig_name = cebra_file_name.replace(".pt", ".png")
-    
-    # save fig to the cebra directory
-    fig_dir = os.path.join(data_dir, 'cebra')
-    fig_path = os.path.join(fig_dir, fig_name)
-    plt.savefig(fig_path)
-
-    pass
-
-
 
 
