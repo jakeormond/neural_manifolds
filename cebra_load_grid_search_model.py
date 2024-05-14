dc0cfb24a45ea32db2c827018bbc5f7555d2d92d
Jake Ormond <j.ormond@ucl.ac.uk>
2024-05-14 14:34:30 +0100

    loading grid search results

--- cebra_load_grid_search_model.py
+++ cebra_load_grid_search_model.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import pandas as pd
 from sklearn.metrics import r2_score
  
 from cebra_embedding_time_grid_search import CustomCEBRA, Logger # needed because I saved the model as a pickle file
@@ -10,69 +11,69 @@ from cebra_embedding_time_grid_search import CustomCEBRA, Logger # needed becaus
 
 def main():   
 
-    animal = 'rat_7'
-    session = '6-12-2019'
+    animals = ['rat_3', 'rat_7', 'rat_8', 'rat_9', 'rat_10']
 
-    model_dir = os.path.join('D:/analysis/og_honeycomb', animal, session, 'cebra_grid_search') 
+    model_dir = os.path.join('D:/analysis/og_honeycomb/models') 
 
-    # find the model pickle file in model_dir; it begins "cebra_grid_search_"
-    model_files = [f for f in os.listdir(model_dir) if f.startswith('grid_search_model_')]
-    if len(model_files) != 1:
-        raise ValueError(f"Expected 1 model file in {model_dir}, found {len(model_files)}")
-    model_file = model_files[0]
+    counter = 0
+    for a in animals:
+        animal_dir = os.path.join(model_dir, a)
 
-    # load the model pickle file
-    model_path = os.path.join(model_dir, model_file)
-    with open(model_path, 'rb') as f:
-        model = pickle.load(f)
+        for z in range(2):
+            if z == 0:
+                grid_dir = os.path.join(animal_dir, 'non_z_scored_spike_trains')
+                title = f'{a}_non_z_scored_spike_trains'
+            else:
+                grid_dir = os.path.join(animal_dir, 'z_scored_spike_trains')
+                title = f'{a}_z_scored_spike_trains'
 
-    # find the best parameters
-    best_params = model.best_params_
-    print(best_params)
+            # find the model pickle file in model_dir; it begins "cebra_grid_search_"
+            model_files = [f for f in os.listdir(grid_dir) if f.startswith('grid_search_model_')]
+            if len(model_files) != 1:
+                raise ValueError(f"Expected 1 model file in {model_dir}, found {len(model_files)}")
+            model_file = model_files[0]
 
-    # find the best score
-    best_score = model.best_score_
-    print(best_score)
+            # load the model pickle file
+            model_path = os.path.join(grid_dir, model_file)
+            with open(model_path, 'rb') as f:
+                model = pickle.load(f)               
 
-    # make a list of all the model parameters evaluated during the grid search
-    all_params = model.cv_results_['params']
+            # find the best parameters
+            best_params = model.best_params_
+            print(best_params)
 
-    ########### make plots of the grid search results ############
-    params = list(best_params.keys())
+            # find the best score
+            best_score = model.best_score_
+            print(best_score)
 
-    for p in params:
-
-        # make a list of the unique values of the parameter p
-        values = list(set([d[p] for d in all_params]))
-
-        # make a list of the mean test scores for each value of the parameter p
-        scores = [d['mean_test_score'] for d in model.cv_results_ if d[p] == values[0]]
-
-        # make a plot of the mean test scores for each value of the parameter p
-        plt.plot(values, scores)
-        plt.xlabel(p)
-        plt.ylabel('mean test score')
-        plt.title(f'mean test score vs {p}')
-        plt.show()
-
-
-
-    # find the best estimator
-    best_estimator = model.best_estimator_
-
+            ##### make dataframe of the best_params #####
+            if counter == 0:
+                grid_results = pd.DataFrame(best_params, index=[title])
+                # add best_score to the dataframe
+                grid_results['best_score'] = best_score
+            else:
+                temp_df = pd.DataFrame(best_params, index=[title])
+                # add best_score to the dataframe
+                temp_df['best_score'] = best_score
+                grid_results = pd.concat([grid_results, temp_df]) 
+            
+            counter += 1
     
+    
+    print(grid_results)
+    pass
 
 
-
-
-
-    pass 
+            
+            
 
 
 
 
 if __name__ == "__main__":
     main()
+
+    pass
 
 
 
